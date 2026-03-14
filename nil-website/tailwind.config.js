146a8ffc014f6ed271a070952c9f5816c74ac4c5
Mikers <snissn@gmail.com>
2026-03-14 08:36:20 -1000

    fix(theme): address review feedback

--- nil-website/tailwind.config.js
+++ nil-website/tailwind.config.js
@@ -3,6 +3,7 @@ import cybergridPreset from "@nil-store/cybergrid-theme/tailwind-preset";
 /** @type {import('tailwindcss').Config} */
 export default {
   presets: [cybergridPreset],
+  darkMode: "class",
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
