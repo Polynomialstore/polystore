0b48882860781593350ddd2413bbbe5610d8bb28
Mikers <snissn@gmail.com>
2026-09-11 14:49:18 -1000

    chain: preserve the V3 singleton verification path

--- polystorechain/x/polystorechain/keeper/retrieval_v3_batch_bench_test.go
+++ polystorechain/x/polystorechain/keeper/retrieval_v3_batch_bench_test.go
@@ -91,3 +91,20 @@ func BenchmarkRetrievalV3CrossSessionBatchFirstAcceptance(b *testing.B) {
 		})
 	}
 }
+
+func BenchmarkRetrievalV3SingleProofFirstAcceptance(b *testing.B) {
+	f, ctx, sessions, samples := openCryptoSessionV3Batch(b, 1)
+	creator := sessions[0].Obligations[0].AssignedProvider
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		run, _ := ctx.CacheContext()
+		_, err := f.g.server.SubmitRetrievalSessionProofV3(run, &types.MsgSubmitRetrievalSessionProofV3{
+			Creator: creator, SessionId: sessions[0].SessionId, Slot: 0,
+			Proofs: []types.RetrievalSampleProofV3{samples[0]},
+		})
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
